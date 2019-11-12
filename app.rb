d33eadb02d5c346b1ef6815a2ead72950ede154e
Tam Vu <vutam_a@hotmail.com>
2019-11-12 02:49:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,5 +9,9 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Daenerys of the House Targaryen, the First of Her Name, The Unburnt, Queen of the Andals, the Rhoynar and the First Men, Queen of Meereen, Khaleesi of the Great Grass Sea, Protector of the Realm, Lady Regent of the Seven Kingdoms, Breaker of Chains and Mother of Dragons"
   end 
+  
+  get '/hometown' do
+    "My hometown is "
+  end 
 
 end
